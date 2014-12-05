86a133312f14f2a9e1301262809837143f3ffd87
Kashif Ahmed <saifallah786@gmail.com>
2014-12-05 02:32:48 +0000

    added bmi calc, display basic profile details,added certain profile hlpers for profile page, plus lots more

--- client/templates/application/profile.js
+++ client/templates/application/profile.js
@@ -5,11 +5,18 @@ Template.profile.helpers({
   lastName: function() {
     return Meteor.user().profile.lastName;
   },
+  latestWeight: function() {
+    var latestWeight = Weights.findOne({userId: Meteor.userId()}, {sort: {updatedAt: -1}}).weight;
+    return latestWeight;
+  },
+    latestHeight: function() {
+    return Meteor.user().profile.startheight;
+  },
   showBmi: function() {
-    var weight = Meteor.user().profile.startweight;
+  var latestWeight = Weights.findOne({userId:   Meteor.userId()}, {sort: {updatedAt: -1}}).weight;
     var height = Meteor.user().profile.startheight;
     var heightSq = height^2
-    var bmi = weight/heightSq;
+    var bmi = latestWeight/heightSq;
     return bmi.toFixed(2);
   }
 });
