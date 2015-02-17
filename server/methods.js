06cce029af86c00cf3161bb21c752d2643059f1e
Kashif Ahmed <saifallah786@gmail.com>
2015-02-17 16:17:07 +0000

    added method to aggregate latest bmi's for all users and return for use by qcDashboard

--- server/methods.js
+++ server/methods.js
@@ -7,10 +7,63 @@ Meteor.methods({
   },
   bmiAggrfx: function() {
 //     var bmis = Meteor.users;
-   var pipeline = [{$group: {_id: "$bmi", usersinBmi : { $sum: 1 }}}];
+//    var pipeline = [{$group: {_id: "$bmi", usersinBmi : { $sum: 1 }}}];
 //         var pipeline = [{$group: {_id: "$weight", weights : { $sum: 1 }}}];
-    var result = Bmi.aggregate(pipeline);
+    
+    var pipeline1 = [
+      { $sort: {userId: 1, updatedAt: 1, bmi: 1}}, 
+      { $group: {
+        _id: "$userId",
+        lastUpdate: { 
+          $last: "$updatedAt"},
+          bmi: {
+            $last: "$bmi"}
+
+      }
+      }
+    ];
+    var result = Bmi.aggregate(pipeline1);
+//     var resultmr = Bmi.mapReduce(map1, reduce1,{ out: "mapreducetest"});
     console.log(result);
+    return result;
+//     var pipeline2 = [
+//       { $sort: {userId: 1, updatedAt: 1, bmi: 1}},
+//       { $group: {
+//         _id: {
+//           "userId": "$userId",
+//           "bmi":    "$bmi"
+//         },
+//         lastUpdate: { 
+//           $last: "$updatedAt"}
+//       }
+//       }
+//     ];
+//     var pipeline2 = [
+//       { $group: {
+//         _id: {
+//           "userId": "$userId",
+//           "bmi":    "$bmi"
+//         },
+//         "bmiCount": { "$sum": 1 }
+//       }},
+//       { "$group": {
+//         "_id": "$_id.userId",
+//         "bmis": {
+//           "$push": {
+//             "bmi": "$_id.bmi",
+//             "count": "$bmiCount"
+//         },
+//       },
+//        "count": { "$sum": "$bmiCount" }
+//       }},
+//       { "$sort": { "count": -1}},
+//     ];
+//     var map1 = function () {
+//       emit(this.userId, this.bmi);
+//     };
+//     var reduce1 = function (keyUserId, valuesBmis) {
+//       return valuesBmis;
+//     };
   },
   updateWeight: function(doc){
     var insertDoc = {
